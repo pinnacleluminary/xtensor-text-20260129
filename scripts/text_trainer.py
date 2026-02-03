0fb21efb37b4b935aff2ee6be0c9c880fc8486f7
Paul Lynch <pinnacleluminary12@gmail.com>
2026-02-03 07:11:23 +0000

    Updated reg rate value.

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -296,7 +296,7 @@ def main():
     )
 
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=0.85162
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=0.95487
     )
 
     args = parser.parse_args()
