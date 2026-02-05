3e8835b7c4c5821951ddbcc6bcb7bd479978b661
Paul Lynch <pinnacleluminary12@gmail.com>
2026-02-05 09:57:20 +0000

    Significantly lower the reg rate to review the stability.

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -296,7 +296,7 @@ def main():
     )
 
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=0.95487
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=0.051487
     )
 
     args = parser.parse_args()
